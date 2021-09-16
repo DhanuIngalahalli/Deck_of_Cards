9aa7e7c06eee54554e84dc9cfa9e77b5d6bdae24
sonupooja <ingalahallipooja@gmail.com>
2021-09-16 19:17:51 +0530

    Welcome to Deck of cards

---
+++ DeckCards/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeckCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Deck of Cards");
+        }
+    }
+}
