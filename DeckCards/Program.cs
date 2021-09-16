d16c29d28e732a87fa903380e9260c922c78caf8
sonupooja <ingalahallipooja@gmail.com>
2021-09-16 19:27:36 +0530

    Adding Deck of cards program

--- DeckCards/Program.cs
+++ DeckCards/Program.cs
@@ -4,9 +4,14 @@ namespace DeckCards
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)  //main method
         {
-            Console.WriteLine("Welcome to Deck of Cards");
+
+            Console.WriteLine("Welcome to Deck Of Cards");
+            Console.WriteLine("\n");
+            Cards cards = new Cards(); //creating object to call cards()
+            cards.CardsCreator();  //calling cards()
+
         }
     }
 }
